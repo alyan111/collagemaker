2d799189987ace5062b5d1f37fe7b050ba411815
alyan111 <alyan.mobipixels@gmail.com>
2024-06-05 14:58:38 +0500

    Added isText with text box and frameCount

---
+++ public/build/assets/Close-CmErL3D8.js
@@ -0,0 +1,1 @@
+import{u as e,m as s}from"./ActionBar-BWsUPliD.js";import{j as o}from"./app-DUCMwQ2M.js";function m(r){return e}const n=s(o.jsx("path",{d:"M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"}),"Close");export{n as C,m as c};
