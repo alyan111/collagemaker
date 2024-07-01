ce335c9c4bc7f8f87bbefdbc0cc2486566a426fc
alyan111 <alyan.mobipixels@gmail.com>
2024-07-01 11:45:44 +0500

    adding chache to templates v2 apis

--- app/Http/Controllers/v2/TemplateController.php
+++ app/Http/Controllers/v2/TemplateController.php
@@ -37,7 +37,7 @@ class TemplateController extends Controller
         $cacheKey = "templates_with_categories_page_{$request->page}";
         $cachedResponse = Redis::get($cacheKey);
         if ($cachedResponse)
-            return response()->json(json_decode($cachedResponse, true));
+            return response()->json(["templates" => json_decode($cachedResponse, true)]);
         $response = [];
         foreach (Category::with(['templates' => function ($query) {
             $query->take(5);
@@ -48,7 +48,7 @@ class TemplateController extends Controller
                 'templates' => TemplateResource::collection($category['templates'])->toArray($request)
             ];
         Redis::setex($cacheKey, 3600 * 24, json_encode($response)); // Cache for 1 day
-        return response()->json($response);
+        return response()->json(["templates" => $response]);
 
         // $response = [];
         // foreach (Category::with(['templates' => function ($query) {
