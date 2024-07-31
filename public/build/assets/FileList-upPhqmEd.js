f9a66e47c96eae90e0a19620b5ce9098410383be
alyan111 <alyan.mobipixels@gmail.com>
2024-07-31 16:06:57 +0500

    adding build for isSquare

--- public/build/assets/FileList-upPhqmEd.js
+++
@@ -1,1 +0,0 @@
-import{j as r,r as s}from"./app-DUCMwQ2M.js";function i(n){const e=t=>`'${t.name}' of size '${t.size}' and type '${t.type}'`;return n.map(t=>r.jsx("li",{children:e(t)},t.name))}const a=({files:n})=>{if(n.length===0)return r.jsx("div",{children:"Nothing to display"});const e=s.useMemo(()=>i(n),[n]);return r.jsx("div",{children:e})};export{a as FileList};
